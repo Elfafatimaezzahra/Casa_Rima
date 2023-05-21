466c47b3676210b863587e10b237e43a8a9150c2
Elfafatimaezzahra <felfahimi@gmail.com>
2023-05-21 12:48:55 +0100

    ajout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import Routing from "./Routes/Routes";
 
 
 function App() {
+
+//   const mongoose = require('mongoose');
+// require('dotenv').config(); 
+
+
+// // Connectez-vous à MongoDB en utilisant Mongoose
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// .then(() => console.log('Connected to MongoDB'))
+// .catch(err => console.error('Failed to connect to MongoDB', err));
+
   return (
     <Routing />
   );
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from "../Template/Header"
 import Footer from "../Template/Footer"
 import HomeBanner from "../Home/Banner"
 import ShowCards from "../Home/ShowCard"
+import offres from '../../datas/logements.json'
 
 
 function Home() {
+    const [query, setQuery] = useState("");
+
+    const keys =  ["title", "location", "tags"]
+
+    // console.log(offres[1])
+
+    const search = (offres)=>{
+        return offres.filter((item) =>
+        keys.some((key) => item[key].toLowerCase().includes(query))
+        );
+    };
     return (
         <>
             <Header />
             <HomeBanner />
-            <ShowCards />
+            <input type="text" placeholder="Search..." className="search" onChange={(e) => setQuery(e.target.value)}/>
+            <ShowCards offres={search(offres)} />
             <Footer />
         </>
     )
